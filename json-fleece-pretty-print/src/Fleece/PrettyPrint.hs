f0de552f9dc5bf87b6ff384250409ea92aa47d9c
David Vollbracht <david@flipstone.com>
2023-03-16 09:08:33 -0500

    Adds json-fleece-pretty-print package

---
+++ json-fleece-pretty-print/src/Fleece/PrettyPrint.hs
@@ -0,0 +1,229 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeFamilies #-}
+
+module Fleece.PrettyPrint
+  ( PrettyPrinter
+  , prettyPrintLazyText
+  , prettyPrintText
+  , prettyPrintString
+  ) where
+
+import qualified Data.DList as DList
+import qualified Data.Foldable as Fold
+import Data.Int (Int64)
+import qualified Data.Map as Map
+import qualified Data.Scientific as Scientific
+import qualified Data.String as String
+import qualified Data.Text as T
+import qualified Data.Text.Lazy as LT
+import qualified Data.Text.Lazy.Builder as LTB
+import qualified Shrubbery
+
+import qualified Fleece.Core as FC
+
+data PrettyPrinter a
+  = PrettyPrinter FC.Name (a -> Pretty)
+
+data Inline
+  = Plain LTB.Builder
+  | FunctionApplication LTB.Builder Inline
+  | Append Inline Inline
+
+instance String.IsString Inline where
+  fromString = Plain . String.fromString
+
+instance Semigroup Inline where
+  (<>) = Append
+
+data Pretty
+  = Inline Inline
+  | Block [Pretty]
+  | Indent Pretty
+
+prettyToBuilder :: LTB.Builder -> Pretty -> LTB.Builder
+prettyToBuilder indentation pretty =
+  case pretty of
+    Inline inline -> indentation <> inlineToBuilder inline <> newline
+    Block pretties -> foldMap (prettyToBuilder indentation) pretties
+    Indent indentedPretty ->
+      prettyToBuilder (indentation <> "  ") indentedPretty
+
+inlineToBuilder :: Inline -> LTB.Builder
+inlineToBuilder inline =
+  case inline of
+    Plain builder -> builder
+    FunctionApplication function contents ->
+      case contents of
+        Plain plainContents -> function <> " " <> plainContents
+        otherContents -> function <> " (" <> inlineToBuilder otherContents <> ")"
+    Append left right ->
+      inlineToBuilder left <> inlineToBuilder right
+
+prettyPrintLazyText :: PrettyPrinter a -> a -> LT.Text
+prettyPrintLazyText (PrettyPrinter _name toPretty) a =
+  LTB.toLazyText (prettyToBuilder "" (toPretty a))
+
+prettyPrintText :: PrettyPrinter a -> a -> T.Text
+prettyPrintText printer =
+  LT.toStrict . prettyPrintLazyText printer
+
+prettyPrintString :: PrettyPrinter a -> a -> String
+prettyPrintString printer =
+  T.unpack . prettyPrintText printer
+
+instance FC.Fleece PrettyPrinter where
+  data Object PrettyPrinter object _a
+    = Object (DList.DList (object -> Pretty))
+
+  newtype Field PrettyPrinter object _a
+    = Field (object -> Pretty)
+
+  newtype AdditionalFields PrettyPrinter object _a
+    = AdditionalFields (object -> Pretty)
+
+  newtype UnionMembers PrettyPrinter _allTypes handledTypes
+    = UnionMembers (Shrubbery.BranchBuilder handledTypes Pretty)
+
+  schemaName (PrettyPrinter name _toBuilder) =
+    name
+
+  number =
+    PrettyPrinter "number" $ \scientific ->
+      case Scientific.toBoundedInteger scientific :: Maybe Int64 of
+        Just n -> showInline n
+        Nothing -> showInline scientific
+
+  text =
+    PrettyPrinter "text" showInline
+
+  boolean =
+    PrettyPrinter "boolean" showInline
+
+  array (PrettyPrinter name itemToPretty) =
+    PrettyPrinter (FC.annotateName name "array") $ \array ->
+      case Fold.toList array of
+        [] -> Inline "[]"
+        nonEmptyList ->
+          Block
+            . map (prefixArrayItem . itemToPretty)
+            $ nonEmptyList
+
+  null =
+    PrettyPrinter "null" showInline
+
+  nullable (PrettyPrinter name renderNotNull) =
+    PrettyPrinter (FC.annotateName name "nullable") $ \mbValue ->
+      renderEither showInline renderNotNull mbValue
+
+  required fieldName accessor (PrettyPrinter _name renderField) =
+    Field $ \object ->
+      prettyPrintField fieldName (renderField (accessor object))
+
+  optional fieldName accessor (PrettyPrinter _name renderField) =
+    Field $ \object ->
+      prettyPrintField fieldName $
+        renderMaybe renderField (accessor object)
+
+  mapField _f (Field renderField) =
+    Field renderField
+
+  additionalFields accessor (PrettyPrinter _name renderField) =
+    AdditionalFields $ \object ->
+      let
+        printField (key, value) =
+          prettyPrintField
+            (T.unpack key)
+            (renderField value)
+
+        fields =
+          map printField
+            . Map.toList
+            . accessor
+            $ object
+      in
+        Block
+          ( Inline "-- additional fields --"
+              : fields
+          )
+
+  constructor _f =
+    Object DList.empty
+
+  field (Object fields) (Field renderField) =
+    Object (DList.snoc fields renderField)
+
+  additional (Object fields) (AdditionalFields renderAdditional) =
+    Object (DList.snoc fields renderAdditional)
+
+  objectNamed name (Object fields) =
+    PrettyPrinter name $ \object ->
+      Block
+        [ Inline (Plain (renderName name))
+        , Indent (Block (map (\f -> f object) (DList.toList fields)))
+        ]
+
+  validateNamed name unvalidate _check (PrettyPrinter _name toPretty) =
+    PrettyPrinter name $ \value ->
+      prefixConstructor
+        (renderName name)
+        (toPretty (unvalidate value))
+
+  boundedEnumNamed name toText =
+    PrettyPrinter name (showInline . toText)
+
+  unionNamed name (UnionMembers branches) =
+    PrettyPrinter name (Shrubbery.dissect (Shrubbery.branchBuild branches))
+
+  unionMemberWithIndex _index (PrettyPrinter _name toPretty) =
+    UnionMembers (Shrubbery.singleBranch toPretty)
+
+  unionCombine (UnionMembers leftBranches) (UnionMembers rightBranches) =
+    UnionMembers (Shrubbery.appendBranches leftBranches rightBranches)
+
+renderName :: FC.Name -> LTB.Builder
+renderName =
+  LTB.fromString . FC.nameUnqualified
+
+newline :: LTB.Builder
+newline =
+  "\n"
+
+showInline :: Show s => s -> Pretty
+showInline =
+  Inline . Plain . LTB.fromString . show
+
+prettyPrintField :: String -> Pretty -> Pretty
+prettyPrintField fieldName prettyField =
+  let
+    fieldNameEquals =
+      Plain (LTB.fromString fieldName) <> " ="
+  in
+    case prettyField of
+      Inline inline -> Inline (fieldNameEquals <> " " <> inline)
+      rendered -> Block [Inline fieldNameEquals, Indent rendered]
+
+renderMaybe :: (a -> Pretty) -> Maybe a -> Pretty
+renderMaybe renderNotNull mbValue =
+  case mbValue of
+    Nothing -> Inline "Nothing"
+    Just a -> prefixConstructor "Just" (renderNotNull a)
+
+renderEither :: (a -> Pretty) -> (b -> Pretty) -> Either a b -> Pretty
+renderEither renderA renderB eitherAB =
+  case eitherAB of
+    Left a -> prefixConstructor "Left" (renderA a)
+    Right b -> prefixConstructor "Right" (renderB b)
+
+prefixConstructor :: LTB.Builder -> Pretty -> Pretty
+prefixConstructor constructor pretty =
+  case pretty of
+    Inline inline -> Inline (FunctionApplication constructor inline)
+    rendered -> Block [Inline (Plain constructor), Indent rendered]
+
+prefixArrayItem :: Pretty -> Pretty
+prefixArrayItem pretty =
+  case pretty of
+    Inline rendered -> Inline ("- " <> rendered)
+    Block [] -> Block [Inline "-"]
+    Block (first : rest) -> Block [prefixArrayItem first, Indent (Block rest)]
+    Indent indented -> Indent (prefixArrayItem indented)
---
+++ json-fleece-pretty-print/test/Spec.hs
@@ -0,0 +1,356 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main
+  ( main
+  ) where
+
+import qualified Data.Map.Strict as Map
+import qualified Data.Text as T
+import qualified Data.Text.Lazy as LT
+import GHC.Stack (withFrozenCallStack)
+import Hedgehog ((===))
+import qualified Hedgehog as HH
+import qualified Hedgehog.Main as HHM
+
+import Fleece.Core ((#+))
+import qualified Fleece.Core as FC
+import qualified Fleece.Examples as Examples
+import qualified Fleece.PrettyPrint as FPP
+
+main :: IO ()
+main =
+  HHM.defaultMain [HH.checkParallel (HH.Group "json-fleece-pretty-print" tests)]
+
+tests :: [(HH.PropertyName, HH.Property)]
+tests =
+  [ ("prop_object", prop_object)
+  , ("prop_boundedEnum", prop_boundedEnum)
+  , ("prop_nullableField", prop_nullableField)
+  , ("prop_jsonText", prop_jsonText)
+  , ("prop_jsonBoolean", prop_jsonBoolean)
+  , ("prop_jsonNumber", prop_jsonNumber)
+  , ("prop_jsonInteger", prop_jsonInteger)
+  , ("prop_jsonEmptyArray", prop_jsonEmptyArray)
+  , ("prop_jsonStringArray", prop_jsonStringArray)
+  , ("prop_jsonObjectArray", prop_jsonObjectArray)
+  , ("prop_jsonObject", prop_jsonObject)
+  , ("prop_jsonNull", prop_jsonNull)
+  , ("prop_validate", prop_validate)
+  , ("prop_optional", prop_optional)
+  , ("prop_optionalNullableFieldEmitNull", prop_optionalNullableFieldEmitNull)
+  , ("prop_optionalNullableFieldOmitKey", prop_optionalNullableFieldOmitKey)
+  , ("prop_additional", prop_additional)
+  , ("prop_nestedObject", prop_nestedObject)
+  ]
+
+prop_object :: HH.Property
+prop_object =
+  HH.withTests 1 . HH.property $
+    let
+      value =
+        Examples.FooBar
+          { Examples.foo = "baz"
+          , Examples.bar = 1.2
+          }
+
+      expected =
+        [ "FooBar"
+        , "  foo = \"baz\""
+        , "  bar = 1.2"
+        ]
+    in
+      assertPrettyPrintEquals Examples.fooBarSchema value expected
+
+prop_boundedEnum :: HH.Property
+prop_boundedEnum =
+  HH.withTests 1 . HH.property $
+    let
+      value =
+        Examples.Apple
+      expected =
+        [ "\"apple\""
+        ]
+    in
+      assertPrettyPrintEquals Examples.boundedEnumSchema value expected
+
+prop_nullableField :: HH.Property
+prop_nullableField =
+  HH.withTests 1 . HH.property $
+    let
+      value =
+        Examples.NullableField
+          { Examples.exampleNullableField = Right "foo"
+          }
+
+      expected =
+        [ "NullableField"
+        , "  nullableField = Right \"foo\""
+        ]
+    in
+      assertPrettyPrintEquals Examples.nullableFieldSchema value expected
+
+prop_jsonText :: HH.Property
+prop_jsonText =
+  HH.withTests 1 . HH.property $
+    assertPrettyPrintEquals
+      FC.anyJSON
+      (FC.mkJSONText "foo")
+      [ "AnyJSON \"foo\""
+      ]
+
+prop_jsonBoolean :: HH.Property
+prop_jsonBoolean =
+  HH.withTests 1 . HH.property $
+    assertPrettyPrintEquals
+      FC.anyJSON
+      (FC.mkJSONBool True)
+      [ "AnyJSON True"
+      ]
+
+prop_jsonNumber :: HH.Property
+prop_jsonNumber =
+  HH.withTests 1 . HH.property $
+    assertPrettyPrintEquals
+      FC.anyJSON
+      (FC.mkJSONNumber 1.33)
+      [ "AnyJSON 1.33"
+      ]
+
+prop_jsonInteger :: HH.Property
+prop_jsonInteger =
+  HH.withTests 1 . HH.property $
+    assertPrettyPrintEquals
+      FC.anyJSON
+      (FC.mkJSONNumber 3)
+      [ "AnyJSON 3"
+      ]
+
+prop_jsonEmptyArray :: HH.Property
+prop_jsonEmptyArray =
+  HH.withTests 1 . HH.property $
+    let
+      value =
+        FC.mkJSONArray []
+
+      expected =
+        [ "AnyJSON ([AnyJSON] [])"
+        ]
+    in
+      assertPrettyPrintEquals FC.anyJSON value expected
+
+prop_jsonStringArray :: HH.Property
+prop_jsonStringArray =
+  HH.withTests 1 . HH.property $
+    let
+      value =
+        FC.mkJSONArray [FC.mkJSONText "foo", FC.mkJSONText "bar"]
+
+      expected =
+        [ "AnyJSON"
+        , "  [AnyJSON]"
+        , "    - AnyJSON \"foo\""
+        , "    - AnyJSON \"bar\""
+        ]
+    in
+      assertPrettyPrintEquals FC.anyJSON value expected
+
+prop_jsonObjectArray :: HH.Property
+prop_jsonObjectArray =
+  HH.withTests 1 . HH.property $
+    let
+      value =
+        FC.mkJSONArray
+          [ FC.mkJSONObject (Map.singleton "foo" (FC.mkJSONText "bar"))
+          , FC.mkJSONObject (Map.singleton "baz" (FC.mkJSONText "bat"))
+          ]
+
+      expected =
+        [ "AnyJSON"
+        , "  [AnyJSON]"
+        , "    - AnyJSON"
+        , "        AnyJSON object"
+        , "          -- additional fields --"
+        , "          foo = AnyJSON \"bar\""
+        , "    - AnyJSON"
+        , "        AnyJSON object"
+        , "          -- additional fields --"
+        , "          baz = AnyJSON \"bat\""
+        ]
+    in
+      assertPrettyPrintEquals FC.anyJSON value expected
+
+prop_jsonObject :: HH.Property
+prop_jsonObject =
+  HH.withTests 1 . HH.property $
+    let
+      value =
+        FC.mkJSONObject $
+          Map.fromList
+            [ ("foo", FC.mkJSONText "bar")
+            , ("baz", FC.mkJSONText "bat")
+            ]
+
+      expected =
+        [ "AnyJSON"
+        , "  AnyJSON object"
+        , "    -- additional fields --"
+        , "    baz = AnyJSON \"bat\""
+        , "    foo = AnyJSON \"bar\""
+        ]
+    in
+      assertPrettyPrintEquals FC.anyJSON value expected
+
+prop_jsonNull :: HH.Property
+prop_jsonNull =
+  HH.withTests 1 . HH.property $
+    assertPrettyPrintEquals
+      FC.anyJSON
+      FC.mkJSONNull
+      [ "AnyJSON Null"
+      ]
+
+prop_validate :: HH.Property
+prop_validate =
+  HH.withTests 1 . HH.property $
+    let
+      value =
+        Examples.Validation "foo"
+      expected =
+        [ "Validation \"foo\""
+        ]
+    in
+      assertPrettyPrintEquals Examples.validationSchema value expected
+
+prop_optional :: HH.Property
+prop_optional =
+  HH.withTests 1 . HH.property $
+    let
+      value =
+        Examples.OptionalField
+          { Examples.exampleOptionalField = Just "foo"
+          }
+
+      expected =
+        [ "OptionalField"
+        , "  optionalField = Just \"foo\""
+        ]
+    in
+      assertPrettyPrintEquals Examples.optionalFieldSchema value expected
+
+prop_optionalNullableFieldEmitNull :: HH.Property
+prop_optionalNullableFieldEmitNull =
+  HH.withTests 1 . HH.property $
+    let
+      value =
+        Examples.OptionalNullableFieldEmitNull
+          { Examples.exampleOptionalNullableFieldEmitNullField = Just "foo"
+          }
+
+      expected =
+        [ "OptionalNullableFieldEmitNull"
+        , "  optionalNullableField = Just (Right \"foo\")"
+        ]
+    in
+      assertPrettyPrintEquals
+        Examples.optionalNullableFieldEmitNullSchema
+        value
+        expected
+
+prop_optionalNullableFieldOmitKey :: HH.Property
+prop_optionalNullableFieldOmitKey =
+  HH.withTests 1 . HH.property $
+    let
+      value =
+        Examples.OptionalNullableFieldOmitKey
+          { Examples.exampleOptionalNullableFieldOmitKeyField = Just "foo"
+          }
+
+      expected =
+        [ "OptionalNullableFieldOmitKey"
+        , "  optionalNullableField = Just (Right \"foo\")"
+        ]
+    in
+      assertPrettyPrintEquals Examples.optionalNullableFieldOmitKeySchema value expected
+
+prop_additional :: HH.Property
+prop_additional =
+  HH.withTests 1 . HH.property $
+    let
+      value =
+        Examples.AdditionalFieldsExample
+          { Examples.field1 = "foo"
+          , Examples.field2 = "bar"
+          , Examples.otherFields =
+              Map.fromList
+                [ ("other1", "baz")
+                , ("other2", "bat")
+                ]
+          }
+      expected =
+        [ "AdditionalFieldsExample"
+        , "  field1 = \"foo\""
+        , "  field2 = \"bar\""
+        , "  -- additional fields --"
+        , "  other1 = \"baz\""
+        , "  other2 = \"bat\""
+        ]
+    in
+      assertPrettyPrintEquals Examples.additionalFieldsExampleSchema value expected
+
+prop_nestedObject :: HH.Property
+prop_nestedObject =
+  HH.withTests 1 . HH.property $
+    let
+      value =
+        Parent
+          { parentField1 = "foo"
+          , parentField2 = "bar"
+          , nestedObject =
+              NestedObject
+                { nestedField1 = "baz"
+                , nestedField2 = "bat"
+                }
+          }
+
+      expected =
+        [ "Parent"
+        , "  field1 = \"foo\""
+        , "  field2 = \"bar\""
+        , "  nested ="
+        , "    NestedObject"
+        , "      field1 = \"baz\""
+        , "      field2 = \"bat\""
+        ]
+    in
+      assertPrettyPrintEquals parentSchema value expected
+
+data Parent = Parent
+  { parentField1 :: T.Text
+  , parentField2 :: T.Text
+  , nestedObject :: NestedObject
+  }
+
+parentSchema :: FC.Fleece schema => schema Parent
+parentSchema =
+  FC.object $
+    FC.constructor Parent
+      #+ FC.required "field1" parentField1 FC.text
+      #+ FC.required "field2" parentField2 FC.text
+      #+ FC.required "nested" nestedObject nestedObjectSchema
+
+data NestedObject = NestedObject
+  { nestedField1 :: T.Text
+  , nestedField2 :: T.Text
+  }
+
+nestedObjectSchema :: FC.Fleece schema => schema NestedObject
+nestedObjectSchema =
+  FC.object $
+    FC.constructor NestedObject
+      #+ FC.required "field1" nestedField1 FC.text
+      #+ FC.required "field2" nestedField2 FC.text
+
+assertPrettyPrintEquals :: FPP.PrettyPrinter a -> a -> [LT.Text] -> HH.PropertyT IO ()
+assertPrettyPrintEquals schema a expected =
+  withFrozenCallStack $
+    LT.lines (FPP.prettyPrintLazyText schema a) === expected
